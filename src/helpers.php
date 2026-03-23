4654294f9deb1e1ea449cc14faf8bf4c14b81f11
SalsaCat <laugessvan@gmail.com>
2026-03-23 15:11:23 +0100

    Improve decoding

--- src/helpers.php
+++ src/helpers.php
@@ -44,7 +44,7 @@ function getPageContent(string $pageSlug, string $sectionKey): string {
         ':section' => $sectionKey,
     ]);
     $row = $stmt->fetch();
-    return html_entity_decode($row['content'] ?? '');
+    return htmlspecialchars_decode($row['content'] ?? '');
 }
 
 function isSudoAdmin(): bool {
